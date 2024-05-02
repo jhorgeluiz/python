40f659c6e0e18014112c910f91bc9ae8dbfe37a2
Jorge Luiz <j87150712@gmail.com>
2024-05-02 15:40:56 -0300

    desafios

--- desafios/desafio022.py
+++ desafios/desafio022.py
@@ -4,3 +4,13 @@ crie um programa que leia o nome completo de uma pessoa e mostre:
 -o nome com todas as letras minusculas.
 -quantas letras ao todo(sem espaços).
 -quantas letras te o primeriro nome. '''
+print('-----------------------------------------')
+
+nome = input('digite seu nome completo: ')
+print("seu nome em maiusculas é: ",nome.upper())
+print('seu o nome em munusculas é: ',nome.lower())
+print('seu nome tem: ',len(nome))
+print('seu primeiro nome é: ',nome[0:6])
+
+
+print('-----------------------------------------')
--- desafios/desafio023.py
+++ desafios/desafio023.py
@@ -6,3 +6,11 @@ dez: 9
 cen: 9
 mil: 1 
 '''
+print('-----------------------------------------')
+ano = input('digite o ano: ')
+print('unidade é: ',ano[0:1])
+print('dezena é: ',ano[1:2])
+print('centena é: ',ano[2:3])
+print('milhar é: ',ano[3:4])
+
+print('-----------------------------------------')
